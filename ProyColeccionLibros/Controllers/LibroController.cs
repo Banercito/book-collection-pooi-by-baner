aa532464a7b21d6c80e7275a93c1ef063c5ea9d9
Banercito <I202333578@cibertec.edu.pe>
2025-08-16 23:58:58 -0500

    Proyecto inicial en C# con CRUD de libros.

---
+++ ProyColeccionLibros/Controllers/LibroController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using ProyColeccionLibros.Models;
+
+namespace ProyColeccionLibros.Controllers
+{
+    public class LibroController : Controller
+    {
+        static List<Libro> lista = new List<Libro>();
+
+        // GET: Libro
+        public ActionResult IndexLibro()
+        {
+            // enviamos los datos del modelo a la vista
+            return View(lista);
+        }
+
+        // GET: Libro/Details/5
+        public ActionResult DetailsLibro(int id)
+        {
+            // buscar un libro en la colección lista
+            // que corresponda en el codigo con el id
+            // enviado
+            Libro buscado = lista.Find( L => L.IdLibro == id );
+
+            // los datos del modelo lo enviamos a la vista
+            return View(buscado);
+        }
+
+        // GET: Libro/Create
+        public ActionResult CreateLibro()
+        {
+            Libro nuevo = new Libro();
+            nuevo.FechaImpresion = DateTime.Today;
+            //
+            return View(nuevo);
+        }
+
+        // POST: Libro/Create
+        [HttpPost]
+        public ActionResult CreateLibro(Libro obj)
+        {
+            try
+            {
+                // agregando un nuevo libro a la coleccion
+                lista.Add(obj);
+                ViewBag.mensaje = "Libro registrado correctamente";
+            }
+            catch(Exception ex)
+            {
+                ViewBag.mensaje = ex.Message;
+            }
+            return View(obj);
+        }
+
+        // GET: Libro/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Libro/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Libro/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Libro/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ProyColeccionLibros/Models/Libro.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ProyColeccionLibros.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProyColeccionLibros.Models
+{
+    public class Libro
+    {
+        [Required]
+        [Range(1001, 2000)]    
+        public int IdLibro { get; set; }
+        
+        [Required]
+        public string NombreLibro { get; set; }
+        
+        [Required]
+        public double Precio { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true,
+           DataFormatString ="{0:yyyy-MM-dd}" )]
+        public DateTime FechaImpresion { get; set; }
+
+        public bool EsNuevo { get; set; }
+    }
+}
